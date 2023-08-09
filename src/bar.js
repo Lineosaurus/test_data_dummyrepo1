2f19559b30452a32a2dc43e0c26a6201a8ef045a
Nicholas Valentinus <nvfastplease@gmail.com>
2023-08-09 07:34:27 -0700

    commit 4

---
+++ src/bar.js
@@ -0,0 +1,24 @@
+// Function to add two numbers
+function add(a, b) {
+    return a + b;
+}
+
+// Variables
+let num1 = 5;
+let num2 = 7;
+
+// Function call and output
+let result = add(num1, num2);
+console.log("Result:", result);
+
+// Array example
+let colors = ["red", "green", "blue"];
+console.log("Colors:", colors);
+
+// Object example
+let person = {
+    name: "Alice",
+    age: 30,
+    profession: "Engineer"
+};
+console.log("Person:", person);
