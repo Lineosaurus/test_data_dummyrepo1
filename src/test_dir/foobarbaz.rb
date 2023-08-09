140a9a6efa08c7ad1ffd31e4671bf2f9ef77071e
Nicholas Valentinus <nvfastplease@gmail.com>
2023-08-09 07:38:45 -0700

    commit 6

---
+++ src/test_dir/foobarbaz.rb
@@ -0,0 +1,26 @@
+# Function to add two numbers
+def add(a, b)
+    a + b
+end
+
+puts "Dummy Ruby File"
+
+# Variables
+num1 = 5
+num2 = 7
+
+# Function call and output
+result = add(num1, num2)
+puts "Result: #{result}"
+
+# Array example
+colors = ["red", "green", "blue"]
+puts "Colors: #{colors}"
+
+# Hash example
+person = {
+    name: "Alice",
+    age: 30,
+    profession: "Engineer"
+}
+puts "Person: #{person}"
